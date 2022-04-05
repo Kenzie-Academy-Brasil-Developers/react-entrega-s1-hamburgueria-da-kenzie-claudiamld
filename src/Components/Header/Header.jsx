1b5a866038e323bc85b5abf842c63b6019b0fac4
Claudia Dias <claudiamld@outlook.com>
2022-04-05 18:51:10 -0300

    Header Component finished

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,13 @@
+import './styles.css'
+
 const Header = () => {
     return(
-        <div>
-            
+        <div className='box-header'>
+            <h1 className="logo-burger">Burger <span className="logo-kenzie">Kenzie</span></h1>
+            <div className='input-box'>
+                <input className="input-header" placeholder="Digitar pesquisa" />
+                <button className="btn-header">Pesquisar</button>
+            </div>
         </div>
     )
 }
